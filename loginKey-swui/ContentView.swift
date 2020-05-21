5465dbc1ea2961d062f5b8d3a8c324ca8ae4db5f
Miguel Tomairo <matomairo@gmail.com>
2020-05-20 23:15:28 -0500

    add stacks

--- loginKey-swui/ContentView.swift
+++ loginKey-swui/ContentView.swift
@@ -29,120 +29,212 @@ struct Home: View {
     
     var body: some View {
         
-        ZStack{
+        ScrollView(.init(), showsIndicators: false) {
             
-            Color.black.opacity(0.03).edgesIgnoringSafeArea(.all)
-            
-            VStack{
-                
-                HStack(alignment: .top, spacing: 0){
-                    
-//                    Image("logo")
-//                        .offset(x: 20, y: 20)
-                    
-                    Image("logo1")
-                }
-                
-                VStack(alignment: .leading){
-                    
-                    Text("Login")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    
-                    Text("Username")
-                        .fontWeight(.bold)
-                        .padding(.top, 20)
-                    
-                    VStack{
-                        TextField("", text: self.$email)
+            ZStack{
                         
-                        Rectangle()
-                        .fill(self.email == "" ? Color.black.opacity(0.08) : Color("Color1"))
-                        .frame(height: 3)
-                    }
-                    
-                    Text("Password")
-                        .fontWeight(.bold)
-                        .padding(.top, 20)
-                    
-                    VStack{
-                        SecureField("", text: self.$pass)
-                        Rectangle()
-                        .fill(self.pass == "" ? Color.black.opacity(0.08) : Color("Color1"))
-                            .frame(height: 3)
-                    }
-                    
-                    HStack{
+                VStack{
                         
-                        Spacer()
-                        
-                        Button(action: {
+                        HStack(alignment: .top, spacing: 0){
                             
-                        }) {
-                            Text("Forget Password?")
-                                .fontWeight(.bold)
-                            .foregroundColor(Color("Color1"))
+        //                    Image("logo")
+        //                        .offset(x: 20, y: 20)
+                            Image("logo1")
                         }
-                    }
-                    .padding(.top)
-                    
-                }
-                .foregroundColor(Color.black.opacity(0.7))
-                .padding()
-                .background(Color.white)
-                .overlay(Rectangle().stroke(Color.black.opacity(0.03), lineWidth: 1)).shadow(radius: 4)
-                .padding(.horizontal)
-                
-                HStack{
-                    
-                    Button(action: {
-                        self.rem.toggle()
-                    }) {
                         
-                        HStack(spacing: 10){
-                            ZStack{
+                        VStack(alignment: .leading){
+                            
+                            Text("Login")
+                                .font(.title)
+                                .fontWeight(.bold)
+
+                            Text("Username")
+                                .fontWeight(.bold)
+                                .padding(.top, 20)
+                            
+                            VStack{
+                                TextField("", text: self.$email)
+                                    .keyboardType(.emailAddress)
+                                Rectangle()
+                                .fill(self.email == "" ? Color.black.opacity(0.08) : Color("Color1"))
+                                .frame(height: 3)
+                            }
+                            
+                            Text("Password")
+                                .fontWeight(.bold)
+                                .padding(.top, 20)
+                            
+                            VStack{
+                                SecureField("", text: self.$pass)
+                                Rectangle()
+                                .fill(self.pass == "" ? Color.black.opacity(0.08) : Color("Color1"))
+                                    .frame(height: 3)
+                            }
+                            
+                            HStack{
                                 
-                                Circle()
-                                    .stroke(LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1")]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
-                                .frame(width: 20, height: 20)
+                                Spacer()
                                 
-                                if self.rem{
+                                Button(action: {
                                     
-                                    Circle()
-                                    .fill(Color("Color1"))
-                                    .frame(width: 10, height: 10)
+                                }) {
+                                    Text("Forget Password?")
+                                        .fontWeight(.bold)
+                                    .foregroundColor(Color("Color1"))
                                 }
                             }
+                            .padding(.top)
+                            
                         }
+                        .foregroundColor(Color.black.opacity(0.7))
+                        .padding()
+                        .background(Color.white)
+                        .overlay(Rectangle().stroke(Color.black.opacity(0.03), lineWidth: 1)).shadow(radius: 4)
+                        .padding(.horizontal)
+                        .padding(.top, -80)
                         
-                        Text("Remenber me")
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.black.opacity(0.7))
+                        HStack{
+                            
+                            Button(action: {
+                                self.rem.toggle()
+                            }) {
+                                
+                                HStack(spacing: 10){
+                                    ZStack{
+                                        
+                                        Circle()
+                                            .stroke(LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1")]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+                                        .frame(width: 20, height: 20)
+                                        
+                                        if self.rem{
+                                            
+                                            Circle()
+                                            .fill(Color("Color1"))
+                                            .frame(width: 10, height: 10)
+                                        }
+                                    }
+                                }
+                                
+                                Text("Remenber me")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.black.opacity(0.7))
+                            }
+                            .padding(.leading, 10)
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                
+                            }) {
+                                
+                                Text("SIGNIN")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .padding(.vertical)
+                                    .padding(.horizontal, 35)
+                                    .background(LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1")]), startPoint: .leading, endPoint: .trailing))
+                                .cornerRadius(5)
+                            }
+                        }
+                        .padding(.top)
+                        .padding(.horizontal)
+                        
+                        HStack{
+                            
+                            Rectangle()
+                                .fill(Color.black.opacity(0.05))
+                                .frame(width: 100, height: 5)
+                            
+                            Text("Social Login")
+                                .foregroundColor(Color.black.opacity(0.7))
+                                .fontWeight(.bold)
+                            
+                            Rectangle()
+                            .fill(Color.black.opacity(0.05))
+                            .frame(width: 100, height: 5)
+                        }
+                        .padding(.top)
+                        
+                        HStack(spacing: 20){
+                            
+                            Button(action: {
+                                
+                            }) {
+                                
+                                Image("google")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width: 35, height: 35)
+                                .padding(8)
+                                .background(Color.red)
+                                .clipShape(Circle())
+                            }
+                            
+                            Button(action: {
+                                
+                            }) {
+                                
+                                Image("twitter")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width: 35, height: 35)
+                                .padding(8)
+                                .background(Color("Color1"))
+                                .clipShape(Circle())
+                            }
+                            
+                            Button(action: {
+                                
+                            }) {
+                                
+                                Image("facebook")
+                                .renderingMode(.original)
+                                .resizable()
+                                .frame(width: 48, height: 48)
+            
+                            }
+                        }
+                        .padding(.top)
+                        
+                        HStack{
+                            
+                            Text("New User?")
+                                .foregroundColor(Color.black.opacity(0.7))
+                                .fontWeight(.bold)
+                            
+                            Button(action: {
+                                
+                            }) {
+                                
+                                Text("SignUp")
+                                .foregroundColor(Color("Color1"))
+                                    .fontWeight(.bold)
+                            }
+                        }
+                        .padding(.top, 20)
+                        .padding(.bottom)
+                        
+                        Spacer()
                     }
-                    .padding(.leading, 10)
-                    
-                    Spacer()
-                    
-                    Button(action: {
+                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
                         
-                    }) {
-                        
-                        Text("SIGNIN")
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .padding(.vertical)
-                            .padding(.horizontal, 35)
-                            .background(LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1")]), startPoint: .leading, endPoint: .trailing))
-                        .cornerRadius(5)
-                    }
-                }
-                .padding(.top)
-                .padding(.horizontal)
-                
-                Spacer()
             }
             
+        }
+        .background(Color.black.opacity(0.03).edgesIgnoringSafeArea(.all))
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
             
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidShowNotification, object: nil, queue: .main) { (not) in
+                
+                
+            }
+            
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidHideNotification, object: nil, queue: .main) { (not) in
+                
+                
+            }
         }
     }
 }
